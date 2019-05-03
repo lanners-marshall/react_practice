be0fda78f2b4b818908c0d6a2cf50f673cf01bdf
lanners-marshall <lanners.marshall@gmail.com>
2019-05-02 20:19:51 -0700

    making top component

--- app/index.js
+++ app/index.js
@@ -1,16 +1,7 @@
 var React = require('react');
 var ReactDom = require('react-dom');
-require('./index.css')
-
-class App extends React.Component {
-	render(){
-		return (
-			<div>
-				Hello everyone!
-			</div>
-		)
-	}
-}
+require('./index.css');
+var App = require('./components/App');
 
 ReactDom.render(
 	<App />,
